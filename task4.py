4c982c56caad9fb6361f8c1d4798b7071d85a394
pockocmoc <pockocmoc@inbox.ru>
2022-12-02 10:17:29 +0300

    First commit

---
+++ task4.py
@@ -0,0 +1,33 @@
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100)
+# многочлена и записать в файл многочлен степени k.
+#
+# Пример:
+#
+# k=2 => 2*x*2 + 4*x + 5 = 0 или x2 + 5 = 0 или 10*x*2 = 0
+
+from random import randint
+
+coefficients = []
+k = int(input('Введите степень многочлена: '))
+for i in range(k + 1):
+    num = randint(- 10, 10)
+    coefficients.append(num)
+
+print(coefficients)
+result = ''
+for i in range(len(coefficients)):
+    if len(result) > 0 and coefficients[i] > 0:
+        result = result + ' + '
+    if coefficients[i] == 0:
+        continue
+
+    result = result + str(coefficients[i]) + 'X^' + str(k - i)
+
+if coefficients[- 1] != 0:
+    result = result[:len(result) - 3]
+result = result + ' = 0'
+print(result)
+
+with open('test.txt', 'w') as f:
+    f.write(result)
---
+++ task5.py
@@ -0,0 +1,33 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+# В file1.txt :
+# 2*x**2 + 4*x + 5
+# В file2.txt:
+# 4*x**2 + 1*x + 4
+# Результирующий файл:
+# 6*x**2 + 5*x + 9
+
+# 5. Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+import io
+from sympy import *
+
+NUM_STEPS = 2
+
+str_sum = ''
+list_source = []
+
+for i in range(1, NUM_STEPS + 1):
+    with io.open(f'file{i}.txt', 'r') as f:
+        list_source.append(f.readline()[:-1])
+
+str_sum = ((list_source[0] + list_source[1])[: - 2])
+print(str_sum)
+
+sum = simplify(str_sum)
+print(sum)
+
+f_out = io.open('file_sum.txt', 'w')
+print(sum, file=f_out)
+f_out.close()
